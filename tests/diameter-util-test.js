61d668387b67b67ab095636accd648b9ce76f53b
rkovacevic <robert.kovacevic1@gmail.com>
2015-09-02 15:08:16 +0000

    Add some tests

--- tests/diameter-util-test.js
+++ tests/diameter-util-test.js
@@ -1,9 +1,11 @@
 jest.autoMockOff();
 jest.dontMock('../lib/diameter-util');
 
+var util = require('../lib/diameter-util');
+
 describe('diameter-util', function() {
+    
     it('generates random number', function() {
-        var util = require('../lib/diameter-util');
         expect(util.random32BitNumber()).toBeGreaterThan(0);
     });
 });
