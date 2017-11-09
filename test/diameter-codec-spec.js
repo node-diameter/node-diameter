66c159e8993db52c2d5493ea87b836c757caacb4
Ates Goral <ates.g@myplanet.com>
2017-11-09 00:59:59 -0500

    Use application name when decoding

--- test/diameter-codec-spec.js
+++ test/diameter-codec-spec.js
@@ -19,14 +19,23 @@ describe('diameter-codec', function() {
             expect(codec.encodeAvp(avp).toString('hex')).toBe('0000010c4000000c000007d1');
         });
 
-        it('encodes string key and string value that is an application name', function() {
+        it('encodes string key and string value that is an application ID name', function() {
             var avp = ['Auth-Application-Id', 'Diameter Credit Control Application'];
             expect(codec.encodeAvp(avp).toString('hex')).toBe('000001024000000c00000004');
         });
 
-        it('encodes string key and string value that is an application code', function() {
+        it('encodes string key and string value that is an application ID code', function() {
             var avp = ['Auth-Application-Id', 4];
             expect(codec.encodeAvp(avp).toString('hex')).toBe('000001024000000c00000004');
+        });
+    });
+    describe('decodeAvp', function() {
+
+        it('decodes application ID code as a string', function() {
+            var buffer = Buffer.from('000001024000000c00000004', 'hex');
+            var decoded = codec.decodeAvp(buffer, 0);
+            expect(decoded.code).toBe('Auth-Application-Id');
+            expect(decoded.data).toBe('Diameter Credit Control Application');
         });
     });
 });
