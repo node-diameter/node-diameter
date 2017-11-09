e1a5ca4d09195608629039720d29eff71142b80b
Ates Goral <ates.g@myplanet.com>
2017-11-08 19:18:04 -0500

    Add support for AppId type

--- test/diameter-codec-spec.js
+++ test/diameter-codec-spec.js
@@ -2,9 +2,31 @@ var codec = require('../lib/diameter-codec');
 
 describe('diameter-codec', function() {
 
-    it('encodes message', function() {
+    describe('enodeAvp', function() {
 
+        it('encodes string key and string value', function() {
+            var avp = ['Result-Code', 'DIAMETER_SUCCESS'];
+            expect(codec.encodeAvp(avp).toString('hex')).toBe('0000010c4000000c000007d1');
+        });
 
-        //expect(util.random32BitNumber()).toBeGreaterThan(0);
+        it('encodes numeric key and string value', function() {
+            var avp = [268, 'DIAMETER_SUCCESS'];
+            expect(codec.encodeAvp(avp).toString('hex')).toBe('0000010c4000000c000007d1');
+        });
+
+        it('encodes numeric key and numeric value', function() {
+            var avp = [268, 2001];
+            expect(codec.encodeAvp(avp).toString('hex')).toBe('0000010c4000000c000007d1');
+        });
+
+        it('encodes string key and string value that is an application name', function() {
+            var avp = ['Auth-Application-Id', 'Diameter Credit Control Application'];
+            expect(codec.encodeAvp(avp).toString('hex')).toBe('000001024000000c00000004');
+        });
+
+        it('encodes string key and string value that is an application code', function() {
+            var avp = ['Auth-Application-Id', 4];
+            expect(codec.encodeAvp(avp).toString('hex')).toBe('000001024000000c00000004');
+        });
     });
 });
